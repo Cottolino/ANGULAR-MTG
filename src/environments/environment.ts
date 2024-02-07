972c5209b5b79b4ad6884dcbd0ff61aff286fc71
Cottolino <cottone.giuseppe.90@gmail.com>
2024-02-07 17:24:56 +0100

    RESPONSIVE

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     apiurlDBPost : 'https://cottoneworking.it/api/MTG-BACKEND/insertCardGot.php',
     apiurlSaveSession : 'https://cottoneworking.it/api/MTG-BACKEND/saveSession.php',
     apiurlLoadSession : 'https://cottoneworking.it/api/MTG-BACKEND/sessions.php',
-    apiurlLoadCardsSession: 'https://cottoneworking.it/api/MTG-BACKEND/CardsSession.php'
+    apiurlLoadCardsSession: 'https://cottoneworking.it/api/MTG-BACKEND/cardsSession.php'
 };
